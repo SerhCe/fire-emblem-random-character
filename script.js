70e93c0f8858ade7942cc10b93fb33ddcd2b5dcb
Serhat Cebba <scebba98@gmail.com>
2025-01-29 23:10:10 +0100

    Add functioning logic

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
-let heroesData = null; 
+let globalDataJson = null; // Global variable to store the fetched data
+let filterDataJson = null; // Json for filtered data
 
 async function loadHeroes() 
 { 
-    const data = await fetch('Heroes.json')
-    const response = await data.json();
+    const dataJson = await fetch('csvjson.json')
+    const response = await dataJson.json();
 
-    console.log(response)
+    console.log(response);
+    return response;
 }
+
+async function start() {
+    globalDataJson = await loadHeroes(); // Load data and store it in the global variable
+    console.log("Loaded data."); // Use the data
+    randomCharacter();
+}
+
+window.onload = start;
+
 
 function randomCharacter() 
 {
-    var characterName = document.getElementById("characterName");
-    var game = document.getElementById("game");
-    var randnr = Math.random();
-    var randjson = getRandomChar();
-    characterName.innerHTML = "test"+randnr;
-    game.innerHTML = "Game!!"+randjson;
-}
+    const characterName = document.getElementById("characterName");
+    const game = document.getElementById("game");
 
+    filterBehaviour();
 
-function getRandomChar() 
-{
-    const keys = Object.keys(jsonObject);
-    const randomKey = keys[Math.floor(Math.random() * keys.length)];
-    const randomValue = jsonObject[randomKey];
+    const randomIndex = Math.floor(Math.random() * Object.keys(filterDataJson).length);
+    const randomEntry = filterDataJson[randomIndex];
+
+    const jsonId = randomEntry.id;
+    const jsonCharId = randomEntry.CharId;
+    const jsonGameId = randomEntry.GameId; 
+    const jsonGame = randomEntry.Game; 
+    const jsonIsMale = randomEntry.IsMale;
+    const jsonIsFemale = randomEntry.isFemale;
     
-    return heroesData.find(hero => hero.charid == 1);
+    characterName.innerHTML = jsonCharId;
+    game.innerHTML = jsonGame; 
 }
 
+function filterBehaviour()
+{
+    filterDataJson = globalDataJson;
+    const gameFilter = parseInt(document.getElementById("gameFilter").value);
+    const maleCharactersFilter= document.getElementById("MaleCharactersFilter").checked;
+    const femaleCharactersFilter= document.getElementById("FemaleCharactersFilter").checked;
 
-loadHeroes();
+    //game Filter logic:
+    if (gameFilter != 0) {
+        filterDataJson = filterDataJson.filter(entry => entry.GameId === gameFilter);
+    }
+
+    if (!maleCharactersFilter) {
+        filterDataJson = filterDataJson.filter(entry => entry.isMale === 0)
+    }
+
+    if (!femaleCharactersFilter) {
+        filterDataJson = filterDataJson.filter(entry => entry.isFemale === 0)
+    }
+
+}
